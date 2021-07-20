bd958b9e70f684513fa3621e3765ef045704a982
Sergio Ramos <sergio.ramos@unemat.br>
2021-07-19 22:04:00 -0400

    list products

---
+++ frontend/components/product/Card.vue
@@ -0,0 +1,82 @@
+<template>
+  <v-card
+    :loading="loading"
+    class="mx-auto my-12"
+    max-width="374"
+  >
+  <template slot="progress">
+    <v-progress-linear
+      color="deep-purple"
+      height="10"
+      indeterminate
+    ></v-progress-linear>
+  </template>
+
+  <v-img
+    height="250"
+    :src="product.image"
+  ></v-img>
+
+  <v-card-title>{{ product.name }}</v-card-title>
+
+  <v-card-text>
+    <v-row
+      align="center"
+      class="mx-0"
+    >
+      <v-rating
+        :value="4.5"
+        color="amber"
+        dense
+        half-increments
+        readonly
+        size="14"
+      ></v-rating>
+
+      <div class="grey--text ms-4">
+        4.5 (413)
+      </div>
+    </v-row>
+
+    <div class="my-4 text-subtitle-1">
+      $ • {{ product.brand }}
+    </div>
+
+    <div>{{ product.description }}</div>
+  </v-card-text>
+
+  <v-divider class="mx-4"></v-divider>
+
+  <v-card-text>
+      <span class="text-h4 deep-orange--text">$ {{ product.price }}</span>
+  </v-card-text>
+
+  <v-card-actions>
+    <v-btn
+      color="green darken-3 white--text"
+      block
+      @click="buy"
+    >
+      Comprar Agora
+    </v-btn>
+  </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+export default {
+  props: ['product'],
+  data: () => ({
+    loading: false,
+    selection: 1,
+  }),
+
+  methods: {
+    buy () {
+      this.loading = true
+
+      setTimeout(() => (this.loading = false), 2000)
+    },
+  },
+}
+</script>
--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,31 +1,49 @@
 <template>
-  <v-row justify="center" align="center">
-    <v-col cols="12" sm="6" md="5">
-      <FiltersSearchAutocomplete
-        :items="products"
-        :value="null"
-      />
-    </v-col>
-    <v-col  cols="12" sm="2" md="1">
-      <FiltersSortSelect
-        :items="['Newest', 'Lowest', 'Highest']"
-        :value="null"
-      />
-    </v-col>
-  </v-row>
+  <div id="home">
+    <v-row justify="center" align="center">
+      <v-col cols="12" sm="6" md="5">
+        <FiltersSearchAutocomplete
+          :items="productItems"
+        />
+      </v-col>
+      <v-col  cols="12" sm="2" md="1">
+        <FiltersSortSelect
+          :items="['Newest', 'Lowest', 'Highest']"
+          :value="null"
+        />
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col
+        cols="12"
+        sm="4"
+        v-for="product in products.data"
+        :key="product.id"
+      >
+        <ProductCard
+          :product="product"
+        />
+      </v-col>
+    </v-row>
+    <v-row>
+      {{ products.data }}
+    </v-row>
+  </div>
 </template>
 
 <script>
 export default {
   data: () => ({
-    productItems: {}
+    productItems: null
   }),
   async asyncData({ $axios }) {
     const products = await $axios.$get('products');
-    const productsItems = products.data.map(function(value, key) {
+    return { products }
+  },
+  created() {
+    this.productItems = this.products.data.map(function (value, key) {
       return {name: value.name, id: value.id};
-    })
-    return { products: productsItems }
+    });
   }
 }
 </script>
